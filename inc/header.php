136a378e6577a6de0dd4946d28b61c67a5645985
Redmic <bilodeau.mic@gmail.com>
2015-11-26 10:29:51 -0500

    Header - revert
    Header - revert

--- inc/header.php
+++ inc/header.php
@@ -2,8 +2,8 @@
   <head>
     <title>Bijouterie Anta950</title>
     <link rel="stylesheet" type="text/css"  href="css/banner.css" />
-    <link rel="stylesheet" type="text/css"  href="css/bootstrap.min.css" />
     <link rel="stylesheet" type="text/css"  href="css/default.css" />
+    <link rel="stylesheet" type="text/css"  href="css/bootstrap.min.css" />
     <link rel="icon"       type="image/png" href="img/logo.png" />
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
