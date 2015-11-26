13b91ac668480382a7a57a3582d217262f39f83b
Redmic <bilodeau.mic@gmail.com>
2015-11-26 12:13:41 -0500

    CSS - Collection
    CSS - Collection

--- collections.php
+++ collections.php
@@ -79,10 +79,8 @@
               <a href="#">
                   <img class="img-responsive" src="img/bague/BA-0001-1.jpg" alt="BA-0001-1">
               </a>
-              <div>
-                <button type="button" class="btn btn-primary">Détails</button>
-                <h3 class="prix">15,00 $</h3>
-              </div>
+              <button type="button" class="btn btn-primary bouton-detail">Détails</button>
+              <h3 class="prix">15,00 $</h3>
           </div>
           <div class="col-md-4 portfolio-item produit">
               <h2>
