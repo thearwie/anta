750fb83f8a9f2b70ab8e6c733438b230fae4232d
pcarranza10 <pcarranza10@hotmail.com>
2015-12-01 18:53:49 -0500

    ok
    ok

--- model/enregistrerFormulaire.php
+++ model/enregistrerFormulaire.php
@@ -23,12 +23,10 @@
       die('Error: ' .mysql_error());
     }
     else 
-   
-       echo "Merci pour s'inscrire<br/>
-             Bijouterie Anta950!<br/><br/>
-             <a href="'http://webc.cegepsherbrooke.qc.ca/~viauma/'">Retour à la page d\'accueil</a>";  
+    {
+       echo "Merci pour s'inscrire";  
       
-    
+    }
     mysql_close($connexionDB);
   }
   else 
