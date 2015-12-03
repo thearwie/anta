f84244fc3e638d933f67a09178562ef4111a10ba
thearwie <maxviau@hotmail.fr>
2015-12-03 15:32:46 -0500

    sync

---
+++ outil.php
@@ -0,0 +1,17 @@
+<?php
+//Retirer
+function formaterTexte($texte = ""){
+  echo "TEST <br/>";
+  while($i<strlen($texte){
+  
+  if($texte[$i] == "\'"){
+    for($y=$i
+  }
+    
+    $i++;
+  }
+  
+}
+
+formaterTexte("test");
+?>
