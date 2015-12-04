0ef43dc195b7fce677a5d2989fa423c8389f3a85
pcarranza10 <pcarranza10@hotmail.com>
2015-12-04 13:39:57 -0500

    sync
    sync

--- model/enregistrerFormulaire.php
+++ model/enregistrerFormulaire.php
@@ -6,37 +6,45 @@
   $password = $_POST['txtPassword']; 
   $adresse = $_POST['txtAdresse']; 
   $codePostal = $_POST['txtCodePostale']; 
-  //$infolettre = '0';
   $ville = '10000'; 
   $infolettre=0;
-  //$infolettreAux = $_POST['checkboxInfolettre'];
   if(isset($_POST['checkboxInfolettre']))
   {
    $infolettre=1;
   }
- 
-   
- 
-  echo $infolettre;
-  
   
     $connexionDB = mysql_connect("webc.cegepsherbrooke.qc.ca", "viauma", "rurove") or die ("Couldn't connect to server");
     
     mysql_select_db("viauma",  $connexionDB) or die ("Couldn't select database");
     
-    $query = "INSERT INTO utilisateur (id, nom, prenom, courriel, motPasse, adresse, codePostal, infolettre, idVille ) VALUES ('10003', ' $nom', '$prenom', ' $courriel', '$password', '$adresse', '$codePostal', '$infolettre', '$ville')";
+    $queryDernierIdUtilisateur = "SELECT MAX(id) as idUtilisateur FROM utilisateur";
     
-
-    if(!mysql_query($query, $connexionDB))
+    
+    
+    $result = mysql_query($queryInsertUtilisateur) or die ('Query failed: '.mysql_error());
+    $total = mysql_num_rows($result);
+    if($total)
     {
-      die('Error: ' .mysql_error());
-    }
-    else 
-    {
-      echo "Merci pour s'inscrire";  
+      $row = mysql_fetch_row($result);
+      $idUtilisateur = $row["idUtilisateur"]+1;      
       
+      
+      
+      if(!mysql_query($queryInsertUtilisateur, $connexionDB))
+      {
+        die('Error: ' .mysql_error());
+      }$queryInsertUtilisateur = "INSERT INTO utilisateur (id, nom, prenom, courriel, motPasse, adresse, codePostal, infolettre, idVille ) VALUES ('$idUtilisateur', ' $nom', '$prenom', ' $courriel', '$password', '$adresse', '$codePostal', '$infolettre', '$ville')";
+      else
+      {
+         echo "Merci pour s'inscrire";  
+      }
+        
+        
+     
+    }
+    else{
+      echo "Pas d'enregistrements...";
     }
     mysql_close($connexionDB);
-
-  
+    
 ?>
