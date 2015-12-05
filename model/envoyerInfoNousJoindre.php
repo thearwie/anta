1f311a28a71ef63c52a4c780c42739496d8cf703
pcarranza10 <pcarranza10@hotmail.com>
2015-12-05 10:14:44 -0500

    sync nous joindre
    sync nous joindre

---
+++ model/envoyerInfoNousJoindre.php
@@ -0,0 +1,15 @@
+﻿<?php 
+
+  $prenom = $_POST['txtPrenom'];
+  $nom = $_POST['txtNom']; 
+  $courriel = $_POST['txtCourriel']; 
+  $message = $_POST['txtAreaCommentaire']; 
+  
+  $from="De: $name<$email>\r\nReturn-path: $email"; 
+  $subject="Message envoyé à partir du formulaire nous joindre"; 
+  mail("email@adress.com", $subject, $message, $from);
+            
+  echo "Message envoyé!<br/><br/><a href='index.php'>Retourner à la page d'accueil</a><br/><a href='nous_joindre.php'>Retourner au formulaire de contact</a>";
+  
+  
+?>
--- nousJoindre.php
+++ nousJoindre.php
@@ -2,7 +2,7 @@
 <!--
 /*******************************************************************************************************/
 /*                                                                                                     */
-/*  Fichier....................:  index.php                                                            */
+/*  Fichier....................:  nousJoindre.php                                                      */
 /*  Type.......................:  Document PHP                                                         */
 /*  Titre......................:  Bijouterie Anta950                                                   */
 /*  Auteurs....................:  ©Paola Marilu Carranza Gonzalez, 2015                                */
@@ -11,7 +11,7 @@
 /*                                                                                                     */
 /*******************************************************************************************************/
 /*                                                                                                     */
-/*  - Page principale du site                                                                          */
+/*  - Page nous joindre                                                                                */
 /*                                                                                                     */
 /*******************************************************************************************************/
 -->
@@ -28,47 +28,104 @@
 <div class='page'>
 
   <div class= "formulaireNousJoindre">
-    <div class = "titreFormulaire">
-      <span class="spanNousJoindre">Laissez-nous vous commentaires</span>	
-    </div>
-	<div class = "label">
-      <span class="infoDetail">* Tous les champs sont obligatoires</span>
-    </div>
-	<div class="prenom">
-	  <div class = "labelFormulaireNousJoindre">
-      <span class="spanNousJoindre">Prénom</span>
-    </div>	
-    <div class = "input">
-      <input type="text" id="txtPrenom" size="40" />
-    </div>
-	</div>
-	<div class="nom">
-		<div class = "labelFormulaireNousJoindre">
-		  <span class="spanNousJoindre">Nom</span>
-		</div>	
-		<div class = "input">
-		  <input type="text" id="txtNom" size="40" />
-		</div>
-	</div>
-	<div class="courriel">
-	  <div class = "labelFormulaireNousJoindre">
-	    <span class="spanNousJoindre">Courriel</span>
-      </div>	
-	  <div class = "input">
-		<input type="text" id="txtCourriel" size="40" />
-	  </div>
-	</div>
-	<div class="message">
-	  <div class = "labelFormulaireNousJoindre">
-	    <span class="spanNousJoindre">Message</span>
-      </div>	
-	  <div class = "inputCommentaire">
-        <textarea rows="8" cols= "60"></textarea>
+  
+    <form id="formulaireNousJoindre" method="POST" enctype="multipart/form-data" action = "model/envoyerInfoNousJoindre.php" > 
+    
+      <div class = "titreFormulaire">
+        <span class="spanNousJoindre">Laissez-nous vous commentaires</span>	
       </div>
-    </div>    
-	  <div class = "buttonEnvoyer">
-      <a class="button">Envoyer</a>
-	  </div>
+      <div class = "label">
+        <span class="infoDetail">* Tous les champs sont obligatoires</span>
+      </div>
+      <div class="prenom">
+        <div class = "labelFormulaireNousJoindre">
+          <span class="spanNousJoindre">Prénom</span>
+        </div>	
+        <div class = "input">
+          <input type="text" id="txtPrenom" size="40" />
+        </div>
+      </div>
+      <div class="nom">
+        <div class = "labelFormulaireNousJoindre">
+          <span class="spanNousJoindre">Nom</span>
+        </div>	
+        <div class = "input">
+          <input type="text" id="txtNom" size="40" />
+        </div>
+      </div>
+      <div class="courriel">
+        <div class = "labelFormulaireNousJoindre">
+          <span class="spanNousJoindre">Courriel</span>
+          </div>	
+        <div class = "input">
+        <input type="text" id="txtCourriel" size="40" />
+        </div>
+      </div>
+      <div class="message">
+        <div class = "labelFormulaireNousJoindre">
+          <span class="spanNousJoindre">Message</span>
+        </div>	
+        <div class = "inputCommentaire">
+          <textarea id="txtAreaCommentaire"  rows="8" cols= "60"></textarea>
+        </div>
+      </div>    
+      <div class = "buttonEnvoyer">
+        <a class="button" href="javascript:{}" onclick = "if(formulaireComplet()){document.getElementById('formulaireNousJoindre').submit();}">Envoyer</a>
+      </div>     
+    </form>
+      
     </div>
   </div>
+  
+  <script type="text/javascript">
+/*****************************************************************************/
+      /* VARIABLES GLOBALES                                                        */
+      /*****************************************************************************/
+      var idControlInput = ['txtPrenom', 'txtNom', 'txtCourriel', 'txtAreaCommentaire', ]; 
+
+      var nomChamp = ['Prénom', 'Nom', 'Courriel', 'Message' ]; 						 
+	  
+     /*=============================================================================*/
+      function formulaireComplet()
+      /*=============================================================================*/
+      {
+        var listeInputVide = 'Remplir le(s) champ(s):\n';	
+        var flagVerifChampsVides = 0;
+        var nombreControl = 0;
+        var idControl = '';
+		 
+        for( nombreControl = 0; nombreControl < 4; nombreControl++)
+        {
+          if(document.getElementById(idControlInput[nombreControl]).value == '')  
+          {	
+            listeInputVide = listeInputVide + nomChamp[nombreControl] +'\n';
+            flagVerifChampsVides = 1;
+          }  
+        }
+		
+        if (flagVerifChampsVides == 1)
+        {
+          alert(listeInputVide);
+          nombreControl = 0;
+		  
+          while(document.getElementById(idControlInput[nombreControl]).value != '')
+          {
+            nombreControl++;
+          }
+          document.getElementById(idControlInput[nombreControl]).focus();
+          return false;
+
+        } 
+        else
+        {
+           return true;
+        }
+        
+      } 
+  
+</script>
+
+  
+  
+  
 <?php include('/inc/footer.php')?>
