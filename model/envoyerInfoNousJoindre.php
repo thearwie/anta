927df536a884dc0c30f4139f959a81b8db795968
pcarranza10 <pcarranza10@hotmail.com>
2015-12-05 10:46:47 -0500

    NJ valider
    NJ valider

--- model/envoyerInfoNousJoindre.php
+++ model/envoyerInfoNousJoindre.php
@@ -1,9 +1,17 @@
 ﻿<?php 
 
-  $prenom = $_POST['txtPrenom'];
-  $nom = $_POST['txtNom']; 
-  $courriel = $_POST['txtCourriel']; 
-  $message = $_POST['txtAreaCommentaire']; 
+  function check_input($data)
+ {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+ }
+  
+  $prenom = check_input($_POST['txtPrenom']);
+  $nom = check_input($_POST['txtNom']); 
+  $courriel = check_input($_POST['txtCourriel']); 
+  $message = check_input($_POST['txtAreaCommentaire']); 
   
   $from="De: $name<$email>\r\nReturn-path: $email"; 
   $subject="Message envoyé à partir du formulaire nous joindre"; 
