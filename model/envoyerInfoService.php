6a15607bdbbb4c9c42367e732c517da066383330
pcarranza10 <pcarranza10@hotmail.com>
2015-12-07 19:27:18 -0500

    Sync Services
    Sync Services

--- model/envoyerInfoService.php
+++ model/envoyerInfoService.php
@@ -1,10 +1,11 @@
-﻿
+
 <?php 
  
   $prenom = $_POST['txtPrenom'];
   $nom = $_POST['txtNom']; 
   $courriel = $_POST['txtCourriel']; 
   $message = $_POST['txtAreaCommentaire']; 
+  $typeService = $_POST['selectService']; 
   
  
   $nomComplet = $prenom." ".$nom;
@@ -16,8 +17,8 @@
   $headers .= "Reply-To:$nomComplet<$courriel>\r\n"; 
  
  
-  $subject="Message du formulaire Nous joindre"; 
-  
+  $subject="Message du formulaire Services - $typeService"; 
+   
   mail("pcarranza10@hotmail.com", $subject, $message, $headers);
             
   echo "Message envoyé!<br/><br/><a href='http://webc.cegepsherbrooke.qc.ca/~viauma/index.php'>Retourner à la page d'accueil</a><br/><a href='http://webc.cegepsherbrooke.qc.ca/~viauma/nousJoindre.php'>Retourner au formulaire nous joindre</a>";
--- services.php
+++ services.php
@@ -2,7 +2,7 @@
 <!--
 /*******************************************************************************************************/
 /*                                                                                                     */
-/*  Fichier....................:  index.php                                                            */
+/*  Fichier....................:  services.php                                                            */
 /*  Type.......................:  Document PHP                                                         */
 /*  Titre......................:  Bijouterie Anta950                                                   */
 /*  Auteurs....................:  ©Paola Marilu Carranza Gonzalez, 2015                                */
@@ -11,7 +11,7 @@
 /*                                                                                                     */
 /*******************************************************************************************************/
 /*                                                                                                     */
-/*  - Page principale du site                                                                          */
+/*  - Page services                                                                                    */
 /*                                                                                                     */
 /*******************************************************************************************************/
 -->
@@ -28,57 +28,115 @@
 <?php include('/inc/header.php')?>
 <div class='page'>
    <div class= "formulaireServices">
-     <div class = "titreFormulaire">
-       <span class="spanServices">Service</span>	
-     </div>
-     <div class="informationFormulaire">
-       <span class="spanServicesInformation">La bijouterie Anta950 offre des services de nettoyage et fabrication de bijoux à mésure (juste pour les produits qui se trouvent dans le catalogue)</span>	
-     </div>
-	   <div class="typeService">
-	     <div class = "labelFormulaireServices">
-	       <span class="spanServices">Type de service</span>
-	     </div>	
-	     <select class = "selectService" name="select">
-         <option class="optionSelectService" value="value1">Nettoyage</option> 
-         <option class="optionSelectService" value="value2" selected>Fabrication à mesure</option>
-       </select>
-     </div>
-	   <div class="prenomService">
-	     <div class = "labelFormulaireServices">
-         <span class="spanServices">Prénom</span>
-       </div>	
-       <div class = "input">
-         <input type="text" id="txtPrenom" size="40" />
+   
+     <form id="formulaireService" method="POST" enctype="multipart/form-data"  action = "model/envoyerInfoService.php" > 
+   
+         <div class = "titreFormulaire">
+           <span class="spanServices">Service</span>	
+         </div>
+         <div class="informationFormulaire">
+           <span class="spanServicesInformation">La bijouterie Anta950 offre des services de nettoyage et fabrication de bijoux à mésure (juste pour les produits qui se trouvent dans le catalogue)</span>	
+         </div>
+         <div class="typeService">
+           <div class = "labelFormulaireServices">
+             <span class="spanServices">Type de service</span>
+           </div>	
+           <select class = "selectService" name="selectService">
+             <option class="optionSelectService" value="value1">Nettoyage</option> 
+             <option class="optionSelectService" value="value2" selected>Fabrication à mesure</option>
+           </select>
+         </div>
+         <div class="prenomService">
+           <div class = "labelFormulaireServices">
+             <span class="spanServices">Prénom</span>
+           </div>	
+           <div class = "input">
+             <input type="text" id="txtPrenom" name="txtPrenom" size="40" />
+           </div>
+         </div>
+       <div class="nomService">
+           <div class = "labelFormulaireServices">
+           <span class="spanServices">Nom</span>
+         </div>	
+         <div class = "input">
+          <input type="text" id="txtNom" name="txtNom" size="40" />
+         </div>
        </div>
-	   </div>
-	 <div class="nomService">
-       <div class = "labelFormulaireServices">
-	     <span class="spanServices">Nom</span>
-	   </div>	
-	   <div class = "input">
-		  <input type="text" id="txtNom" size="40" />
-	   </div>
-	 </div>
-	 <div class="courrielService">
-	   <div class = "labelFormulaireServices">
-	     <span class="spanServices">Courriel</span>
-       </div>	
-	   <div class = "input">
-		 <input type="text" id="txtCourriel" size="40" />
-	   </div>
-	 </div>
-	 <div class="messageService">
-	   <div class = "labelFormulaireServices">
-	     <span class="spanServices">Message</span>
-       </div>	
-	   <div class = "inputCommentaire">
-         <textarea rows="8" cols= "60"></textarea>
+       <div class="courrielService">
+         <div class = "labelFormulaireServices">
+           <span class="spanServices">Courriel</span>
+           </div>	
+         <div class = "input">
+         <input type="text" id="txtCourriel" name="txtCourriel" size="40" />
+         </div>
        </div>
-     </div>    
-	 <div class = "buttonEnvoyer">
-       <a class="button">Envoyer</a>
-	 </div>
-	   
+       <div class="messageService">
+         <div class = "labelFormulaireServices">
+           <span class="spanServices">Message</span>
+           </div>	
+         <div class = "inputCommentaire">
+             <textarea  id="txtAreaCommentaire" name="txtAreaCommentaire" rows="8" cols= "60"></textarea>
+           </div>
+         </div>    
+       <div class = "buttonEnvoyer">
+           <a class="button" href="javascript:{}" onclick = "if(formulaireComplet()){document.getElementById('formulaireService').submit();}" >Envoyer</a>
+       </div>
+       
+	   </form>
    </div>
 </div>
+
+<script type="text/javascript">
+  
+  /*****************************************************************************/
+  /* VARIABLES GLOBALES                                                        */
+  /*****************************************************************************/
+      var idControlInput = ['txtPrenom', 'txtNom', 'txtCourriel', 'txtAreaCommentaire']; 
+
+      var nomChamp = ['Prénom', 'Nom', 'Courriel', 'Message' ]; 						 
+	  
+     /*=============================================================================*/
+      function formulaireComplet()
+      /*=============================================================================*/
+      {
+        var listeInputVide = 'Remplir le(s) champ(s):\n';	
+        var flagVerifChampsVides = 0;
+        var nombreControl = 0;
+        var idControl = '';
+		 
+        for( nombreControl = 0; nombreControl < 4; nombreControl++)
+        {
+          if(document.getElementById(idControlInput[nombreControl]).value == '')  
+          {	
+            listeInputVide = listeInputVide + nomChamp[nombreControl] +'\n';
+            flagVerifChampsVides = 1;
+          }  
+        }
+		
+        if (flagVerifChampsVides == 1)
+        {
+          alert(listeInputVide);
+          nombreControl = 0;
+		  
+          while(document.getElementById(idControlInput[nombreControl]).value != '')
+          {
+            nombreControl++;
+          }
+          document.getElementById(idControlInput[nombreControl]).focus();
+          return false;
+
+        } 
+        else
+        {
+           
+           return true;
+        }
+        
+      } 
+  
+</script>
+
+
+
+
 <?php include('/inc/footer.php')?>
