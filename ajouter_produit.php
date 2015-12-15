36576f4cbca26ffcebf9dda195b70e9b4dc7b038
thearwie <maxviau@hotmail.fr>
2015-12-15 07:18:30 -0500

    ajout

---
+++ ajouter_produit.php
@@ -0,0 +1,157 @@
+<form action="ajouter_produit.php">
+        
+        <br/>
+        <ul class="ul_horizontal">
+          <li><input type="radio" name="produit" value="bague" class="toggle_btn">Bague</li>
+          <li><input type="radio" name="produit" value="collier" class="toggle_btn">Collier</li>
+          <li><input type="radio" name="produit" value="boucleOreille" class="toggle_btn">Boucle d'oreille</li>
+          <li><input type="radio" name="produit" value="bracelet" class="toggle_btn">Bracelet</li>
+        </ul>
+        <br/>
+
+        <div class="conteneur">
+          <div class="cellule">
+            <div class="image_cellule">
+              <div style="width: 150px; height: 100px; background-color: #0000ff;"></div>
+            </div>
+            <div class="checkbox_cellule">
+              <input type="radio" id="nouveau" name="test" value="Nouveau">
+              <label for="nouveau">Nouveau</label>
+            </div>
+          </div>
+          <div class="cellule">
+            <div class="image_cellule">
+              <div style="width: 150px; height: 100px; background-color: #ff0000;"></div>
+            </div>
+            <div class="checkbox_cellule">
+              <input type="radio" id="test1" name="test" value="Diamand">
+              <label for="test1">Diamand</label>
+            </div>
+          </div>
+          <div class="cellule">
+            <div class="image_cellule">
+              <div style="width: 150px; height: 100px; background-color: #ff0000;"></div>
+            </div>
+            <div class="checkbox_cellule">
+              <input type="radio" id="test2" name="test" value="Diamand1">
+              <label for="test2">Diamand</label>
+            </div>
+          </div>
+          <div class="cellule">
+            <div class="image_cellule">
+              <div style="width: 150px; height: 100px; background-color: #ff0000;"></div>
+            </div>
+            <div class="checkbox_cellule">
+              <input type="radio" id="test3" name="test" value="Diamand2">
+              <label for="test3">Diamand</label>
+            </div>
+          </div>
+          <div class="cellule">
+            <div class="image_cellule">
+              <div style="width: 150px; height: 100px; background-color: #ff0000;"></div>
+            </div>
+            <div class="checkbox_cellule">
+              <input type="radio" id="test4" name="test" value="Diamand3">
+              <label for="test4">Diamand</label>
+            </div>
+          </div>
+          <div class="cellule">
+            <div class="image_cellule">
+              <div style="width: 150px; height: 100px; background-color: #ff0000;"></div>
+            </div>
+            <div class="checkbox_cellule">
+              <input type="radio" id="test5" name="test" value="Diamand4">
+              <label for="test5">Diamand</label>
+            </div>
+          </div>
+        </div>
+        
+        <div class="groupe">
+          <!-- <div class="colonneGauche"> -->
+          <div class="colonne">
+            <label>Code</label>
+            <br/>
+            <input type="text" name="code">
+            <br/><br/>
+
+            <label>Matériel principal</label>
+            <br/>
+            <select>
+            <option value="Argent 950" selected>Argent 950</option>
+            <option value="Argent 925">Argent 925</option>
+            <option value="or 14k">Or 14k</option>
+            </select>
+            <br/><br/>
+
+            <label>Autres matériaux</label>
+            <br/>
+            <!--<select size="3" multiple>
+            <option value="diamand">Diamand</option>
+            <option value="rubis">Rubis</option>
+            <option value="zircon">zircon</option>
+            </select>-->
+            <div class="wrapper">
+              <!--<input type="checkbox" name="diamand" value="Diamand">Diamand <br/>
+              <input type="checkbox" name="rubis" value="Rubis">Rubis <br/>
+              <input type="checkbox" name="zircon" value="Zircon">Zircon <br/>
+              <input type="checkbox" name="diamand" value="Diamand">Diamand <br/>
+              <input type="checkbox" name="rubis" value="Rubis">Rubis <br/>
+              <input type="checkbox" name="zircon" value="Zircon">Zircon <br/>
+              <input type="checkbox" name="diamand" value="Diamand">Diamand <br/>
+              <input type="checkbox" name="rubis" value="Rubis">Rubis <br/>
+              <input type="checkbox" name="zircon" value="Zircon">Zircon <br/>
+              <input type="checkbox" name="diamand" value="Diamand">Diamand <br/>
+              <input type="checkbox" name="rubis" value="Rubis">Rubis <br/>
+              <input type="checkbox" name="zircon" value="Zircon">Zircon <br/>-->
+
+              <!-- <div class="colonneGauche"> -->
+              <div class="colonne">
+                <label class="lblCheckbox" for="diamand">Diamand</label>
+                <label class="lblCheckbox" for="rubis">Rubis</label>
+                <label class="lblCheckbox" for="zircon">Zircon</label>
+              </div>
+              <div>
+                <input type="checkbox" id="diamand" name="diamand" value="Diamand"><br/>
+                <input type="checkbox" id="rubis" name="rubis" value="Rubis"><br/>
+                <input type="checkbox" id="zircon" name="zircon" value="Zircon"><br/>
+              </div>
+            </div>
+          </div>
+          <!-- <div class="colonneDroite"> -->
+          <div class="colonne">
+            <label>Image</label> 
+            <br/>
+            <input type="button" id="parcourir" name="parcourir" value="parcourir">
+          </div>
+          <div class="colonne">
+            <div id="photo_produit"></div>
+          </div>
+        </div>
+        <div class="groupe" style="padding: 10px; padding-left: 0px;">
+          <!-- <div class="colonneGauche"> -->
+          <div class="colonne">
+            <label>Prix ($)</label>
+            <br/>
+            <input type="text" name="prix">
+            <br/>
+            <br/>
+          </div>
+          <!-- <div class="colonneDroite"> -->
+          <div class="colonne">
+            <label>Produit en vente?</label>
+            <br/>
+            <input type="radio" id="rBtnOui" name="enVente" value="oui"><label for="rBtnOui">Oui</label>
+            <input type="radio" id="rBtnNon" name="enVente" value="non"><label for="rBtnNon">Non</label><br/>
+          </div>
+        </div>
+        <br/>
+
+        <label>Quantité</label>
+        <br/>
+        <input type="number" name="quantite" value="0">
+
+        <br/>
+        <br/>
+        <input type="button" name="enregistrer" value="Enregistrer">
+        <input type="button" name="annuler" value="Annuler">
+      </form>
---
+++ gestion_produit.php
@@ -0,0 +1,45 @@
+﻿<!DOCTYPE html>
+<!--
+/*******************************************************************************************************/
+/*                                                                                                     */
+/*  Fichier....................:  gestion_produit.php                                                  */
+/*  Type.......................:  Document PHP                                                         */
+/*  Titre......................:  Bijouterie Anta950                                                   */
+/*  Auteurs....................:  ©Maxime Viau, 2015                                                   */
+/*  Date de création...........:  2015-11-16                                                           */
+/*  Date de mise en production.:                                                                       */
+/*                                                                                                     */
+/*******************************************************************************************************/
+/*                                                                                                     */
+/*  - Page de gestion des produits                                                                     */
+/*                                                                                                     */
+/*******************************************************************************************************/
+-->
+<html>
+  <head>
+    <title>Gestion des produits</title>
+
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="description" content="Site de vente de la bijouterie avec exposition des créations">
+    <meta name="keywords" content="Bijou, bague, collier, boucle d'oreille, bracelet">
+    <meta name="author" content="Michaël Bilodeau, Paola Carranza, Maxime Viau">
+
+    <link rel="stylesheet" type="text/css"  href="css/admin.css" />
+  </head>
+  <body>
+    <div class='page'>
+      <h1>Gestion des produits</h1>
+
+      <label>Action</label>
+      <br/>
+      <select>
+        <option value="ajouter" selected>Ajouter</option>
+        <option value="modifier/supprimer">Modifier/Supprimer</option>
+      </select>
+      <br/>
+      
+      <?php include("ajouter_produit.php")?>
+
+  </div>
+  </body>
+</html>
