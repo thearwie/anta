f990af171eeba23cb6906d2363f88f115bc69184
pcarranza10 <pcarranza10@hotmail.com>
2015-12-18 16:05:11 -0500

    test
    test

--- login.php
+++ login.php
@@ -21,7 +21,7 @@
 		<input type="text" id="courriel" name="Courriel" placeholder="courriel" size="60" ><br>
 		<label for="password">Mot de passe</label><br>
 		<input type="password" id="password" name="Password" placeholder="mot de passe" size="60"><br>
-		<input type="submit" name="ouvrirSession" value="OuvrirSession" class="ouvrirSession">
+		<input type="submit" name="ouvrirSession" value="Ouvrir Session" class="ouvrirSession">
 	</form>
 	</section>
 </body>
