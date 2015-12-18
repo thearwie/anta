22b0750f07cb923ff94d98962d73b97e84b428a4
pcarranza10 <pcarranza10@hotmail.com>
2015-12-18 17:43:14 -0500

    test
    test

--- panier.php
+++ panier.php
@@ -51,7 +51,8 @@
 		  $nom = "";
 		  $prix=0;
 		  $image="";
-		  $re = mysql_query("select * from produit where id='.$_GET['id'].'");
+		 // $re = mysql_query("select * from produit where id='".$_GET['id']."'");
+		  $re = mysql_query("select * from produit where id=".$_GET['id']);
 		  while ($f=mysql_fetch_array($re)){
 			$id= $f['id'];
 			$nom= $f['nom'];
