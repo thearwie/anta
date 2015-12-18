c0be1d22bf48798de2eb041d169d72bbd5a91577
pcarranza10 <pcarranza10@hotmail.com>
2015-12-18 16:26:04 -0500

    sync
    sync

--- inc/header.php
+++ inc/header.php
@@ -6,9 +6,7 @@
     <link rel="stylesheet" type="text/css"  href="css/bootstrap.min.css" />
     <link rel="stylesheet" type="text/css"  href="css/smoothproducts.css">
     <link rel="icon"       type="image/png" href="img/logo.png" />
-    <link rel="stylesheet" type="text/css" href="css/stylePanier.css">
-	
-	
+
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
